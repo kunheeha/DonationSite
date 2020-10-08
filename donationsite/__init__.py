1412035ee5d1303b1f55586d5e6b17a026b64840
Kunhee Ha <kunheeha@gmail.com>
2020-10-08 23:10:43 +0100

    made database

--- donationsite/__init__.py
+++ donationsite/__init__.py
@@ -1,11 +1,24 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
-from flask_login import LoginManager, current_user
+from flask_login import LoginManager, current_user, UserMixin
 from flask_admin import Admin, AdminIndexView
+
+
+# class MyAdminIndexView(AdminIndexView):
+#     def is_accessible(self):
+#         if current_user.is_authenticated and current_user.has_role('Admin'):
+#             return True
+#         else:
+#             return False
+
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '91da41a4b94ec02afceef24cd35a8d54'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+# login_manager = LoginManager(app)
 
 
 from donationsite import routes
---
+++ donationsite/models.py
@@ -0,0 +1,116 @@
+from donationsite import db
+from flask_login import UserMixin, current_user
+from flask_admin import AdminIndexView
+from flask_admin.contrib import sqla
+from flask_admin.contrib.sqla import ModelView
+
+
+class User(db.Model, UserMixin):
+    __tablename__ = 'user'
+    id = db.Column(db.Integer(), primary_key=True)
+    name = db.Column(db.String(30), nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    image_file = db.Column(db.String(20), nullable=False,
+                           default='default.jpg')
+    password = db.Column(db.String(60), nullable=False)
+    cv_file = db.Column(db.String(20), nullable=True)
+    goals = db.Column(db.String(240), nullable=True)
+    self_desc = db.Column(db.String(240), nullable=True)
+    degree = db.relationship('Degree', secondary='user_degree')
+    bank_details = db.relationship('Bankdetails', secondary='user_bankdetails')
+    roles = db.relationship('Role', secondary='user_roles')
+
+    def has_role(self, *args):
+        return set(args).issubset({role.name for role in self.roles})
+
+    def __repr__(self):
+        return f"User('{self.name}', '{self.email}', '{self.image_file}')"
+
+
+class Role(db.Model):
+    __tablename__ = 'roles'
+    id = db.Column(db.Integer(), primary_key=True)
+    name = db.Column(db.String(50), unique=True)
+
+    def __repr__(self):
+        return f"Role('{self.name}')"
+
+
+class UserRoles(db.Model):
+    __tablename__ = 'user_roles'
+    id = db.Column(db.Integer(), primary_key=True)
+    user_id = db.Column(db.Integer(), db.ForeignKey(
+        'user.id', ondelete='CASCADE'))
+    role_id = db.Column(db.Integer(), db.ForeignKey(
+        'roles.id', ondelete='CASCADE'))
+
+    def __repr__(self):
+        return f"UserRoles('{self.user_id}', '{self.role_id}')"
+
+
+class Degree(db.Model):
+    __tablename__ = 'degree'
+    id = db.Column(db.Integer(), primary_key=True)
+    title = db.Column(db.String(120), nullable=False)
+    institution = db.Column(db.String(120), nullable=False)
+    grad_year = db.Column(db.String(4), nullable=False)
+
+    def __repr__(self):
+        return f"Degree('{self.title}')"
+
+
+class UserDegree(db.Model):
+    __tablename__ = 'user_degree'
+    id = db.Column(db.Integer(), primary_key=True)
+    user_id = db.Column(db.Integer(), db.ForeignKey(
+        'user.id', ondelete='CASCADE'))
+    degree_id = db.Column(db.Integer(), db.ForeignKey(
+        'degree.id', ondelete='CASCADE'))
+
+    def __repr__(self):
+        return f"UserDegree('{self.user_id}')"
+
+
+class Bankdetails(db.Model):
+    __tablename__ = 'bankdetails'
+    id = db.Column(db.Integer(), primary_key=True)
+    account_holder = db.Column(db.String(30), nullable=False)
+    account_number = db.Column(db.Integer(), unique=True, nullable=False)
+    sort_code = db.Column(db.Integer(), unique=True, nullable=False)
+
+    def __repr__(self):
+        return f"Bankdetails('{self.account_holder}')"
+
+
+class UserBankdetails(db.Model):
+    __tablename__ = 'user_bankdetails'
+    id = db.Column(db.Integer(), primary_key=True)
+    user_id = db.Column(db.Integer(), db.ForeignKey(
+        'user.id', ondelete='CASCADE'))
+    bankdetails_id = db.Column(db.Integer(), db.ForeignKey(
+        'bankdetails.id', ondelete='CASCADE'))
+
+    def __repr__(self):
+        return f"UserBankdetails('{self.user_id}')"
+
+
+# class MyAdminIndexView(AdminIndexView):
+#     def is_accessible(self):
+#         if current_user.is_authenticated and current_user.has_role('Admin'):
+#             return True
+#         else:
+#             return False
+
+
+# class MyModelView(ModelView):
+#     def is_accessible(self):
+#         if current_user.is_authenticated and current_user.has_role('Admin'):
+#             return True
+#         else:
+#             return False
+
+
+# admin.add_view(MyModelView(User, db.session))
+# admin.add_view(MyModelView(Role, db.session))
+# admin.add_view(MyModelView(Degree, db.session))
+# admin.add_view(MyModelView(Bankdetails, db.session))
